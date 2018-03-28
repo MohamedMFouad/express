4573f0788c2e7f0d2bc8a1404b6eaba6acd91178
admin1 <MohamedMFouad>
2018-03-28 11:50:45 +0200

    post massage

--- server.js
+++ server.js
@@ -1,17 +1,27 @@
-const express = require('express');
+const express = require('express')
+var bodyParser = require('body-parser')
 var app = express()
+
+app.use(express.static(__dirname))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 
 
 var messages = [
     {name:'Mohamed', message: 'hello'},
     {name:'Mahmoud', message: 'iam here'}
 ]
-
-app.get('/messages',(req, res)=>{
+app.get('/messages', (req, res)=>{
     res.send(messages)
-});
+})
+app.post('/messages', (req, res) =>{
+   console.log(req.body)
+    messages.push(req.body)
+    res.sendStatus(200);
+})
 
-app.use(express.static(__dirname));
+
 var server = app.listen(3000, ()=>{
     console.log('server is listening on port', server.address().port);
 });
+
