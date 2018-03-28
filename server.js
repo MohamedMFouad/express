94be7b869d4d59092c90d53f4c31f5679bf5e699
admin1 <MohamedMFouad>
2018-03-28 09:01:34 +0200

    html view

--- server.js
+++ server.js
@@ -2,6 +2,15 @@ const express = require('express');
 var app = express()
 
 
+var messages = [
+    {name:'Mohamed', message: 'hello'},
+    {name:'Mahmoud', message: 'iam here'}
+]
+
+app.get('/messages',(req, res)=>{
+    res.send(messages)
+});
+
 app.use(express.static(__dirname));
 var server = app.listen(3000, ()=>{
     console.log('server is listening on port', server.address().port);
